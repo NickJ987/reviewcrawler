58400c3983c42099aa80151e013d6dc0e3f02d39
njordan@gapintelligence.com <NJordan10@gmail.com>
2015-08-07 18:22:45 -0700

    Initial commit of review crawler

---
+++ reviewcrawler.rb
@@ -0,0 +1,9 @@
+class ReviewCrawler
+	attr_reader: :retailer, :sku, :partnumber
+	def inititialize(retailer, sku, partnumber)
+		@retailer = retailer
+		@sku = sku
+		@partnumber = partnumber
+	end
+
+end
